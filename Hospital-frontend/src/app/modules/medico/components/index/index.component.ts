73b1d2cbdb964ac601752f95c67d379457d4ae75
lautarosartor <lautarosartor3@gmail.com>
2024-04-15 18:41:06 -0300

    Reinstalacion de Angular, Angular Material, bootstrap y ngx-toastr, implementamos algunas carpetas

--- Hospital-frontend/src/app/modules/medico/components/index/index.component.ts
+++ Hospital-frontend/src/app/modules/medico/components/index/index.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-index',
-  standalone: true,
-  imports: [],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
--- Hospital-frontend/src/app/modules/medico/medico.module.ts
+++ Hospital-frontend/src/app/modules/medico/medico.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IndexComponent } from './components/index/index.component';
+import { FormComponent } from './components/form/form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent,
+    FormComponent
+  ],
   imports: [
     CommonModule
   ]
